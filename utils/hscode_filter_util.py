4986999e8515558cdf431a5ccc4a28a45da59e4e
ddatta <ddatta@vt.edu>
2021-02-15 22:17:44 -0500

    update

--- utils/hscode_filter_util.py
+++ utils/hscode_filter_util.py
@@ -1,21 +1,25 @@
-VALID_HSCODE_LIST = [
-    '9401', '9403', '9201', '9614', '9202',
-    '9302', '9304', '0305', '8211', '6602',
-    '8201', '9207', '9504', '9205', '9206',
-    '9209', '9202'
-]
+import os
+import sys
+VALID_HSCODE_LIST = []
+with open('./../DomainData/valid_HSCode.txt','r') as fh:
+    VALID_HSCODE_LIST = fh.readlines()
+VALID_HSCODE_LIST = [_.strip('\n') for _ in VALID_HSCODE_LIST]
 
+print(VALID_HSCODE_LIST)
 
 def HSCode_filter_aux(val):
     global VALID_HSCODE_LIST
+    vals = val.split(';')
+    for val in vals:
+        val = str(val[0])
+        val = val.replace('.', '')
+        val = str(val[:6])
+        if val[:2] == '44':
+            return val
+        elif val in VALID_HSCODE_LIST:
+            return val
+        else:
+            continue
 
-    val = val.split(';')
-    val = str(val[0])
-    val = val.replace('.', '')
-    val = str(val[:6])
-    if val[:2] == '44':
-        return val
-    elif val[:4] in VALID_HSCODE_LIST:
-        return str(val[:6])
-    else:
-        return None
+    return None
+
