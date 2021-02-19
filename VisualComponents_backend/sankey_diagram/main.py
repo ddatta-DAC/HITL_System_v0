4887cf8b26ef7a6a17cc996bc3cd1b4e2f1d3812
Debanjan Datta <ddatta@vt.edu>
2021-02-19 16:54:52 -0500

    update

--- VisualComponents_backend/sankey_diagram/main.py
+++ VisualComponents_backend/sankey_diagram/main.py
@@ -14,6 +14,15 @@ sys.path.append('./../..')
 from DB_Ingestion.sqlite_engine import sqlite
 import plotly.graph_objects as go
 SQL_Conn = sqlite.get_engine()
+redis_obj = redisUtil.redisStore
+
+def initialize(
+    DATA_LOC=None,
+    subDIR=None
+):
+    global redis_obj
+    redis_obj.ingest_record_data(DATA_LOC, subDIR)
+    return 
 
 def obtain_data(
         PanjivaRecordID,
@@ -22,6 +31,7 @@ def obtain_data(
         diagram_type=1
 ):
     global SQL_Conn
+    global redis_obj
     if diagram_type == 1:
         domain_subset = [
             'ShipmentOrigin', 'PortOfLading', 'HSCode', 'PortOfUnlading', 'ShipmentDestination'
@@ -31,18 +41,20 @@ def obtain_data(
             'ConsigneePanjivaID', 'HSCode', 'ShipperPanjivaID'
         ]
 
-    redis_obj = redisUtil.redisStore
+    
     record = redis_obj.fetch_data(
-        int(PanjivaRecordID)
+        str(int(PanjivaRecordID))
     )
-
+  
     tmp = pd.read_csv(os.path.join(DATA_LOC, subDIR, 'HSCode.csv'), index_col=None)
-    hscode_value = tmp.loc[tmp['entity_ID'] == record['HSCode']]['value']
-
+    print( tmp.loc[tmp['entity_ID'] == record['HSCode']])
+    
+    hscode_value = tmp.loc[tmp['entity_ID'] == record['HSCode']]['value'].values[0]
+    
+    print('HSCode ', hscode_value)
+   
     if diagram_type == 1:
-        _query_str = 'select {} from Records where HSCode={}'.format(
-            ','.join(domain_subset), hscode_value
-        )
+        _query_str = 'select {} from Records where HSCode={}'.format(','.join(domain_subset), hscode_value)
 
     elif diagram_type == 2:
 
@@ -50,7 +62,7 @@ def obtain_data(
                      'JOIN ConsigneePanjivaID  on  ConsigneePanjivaID.ConsigneePanjivaID=Records.ConsigneePanjivaID ' \
                      'JOIN ShipperPanjivaID on ShipperPanjivaID.ShipperPanjivaID=Records.ShipperPanjivaID ' \
                      'where  HSCode={}'.format(hscode_value)
-
+    print(_query_str)
     # ------------------------------
     # Use the sqllite  DB to read in all records that match HSCode
     # ------------------------------
@@ -98,18 +110,20 @@ def get_sankey_diagram(
     # replace in df
     df_1 = df.copy(deep=True)
     for col in list(df_1.columns):
-        df_1[col] = df_1.parallel_apply( lambda x: label2id[x])
+        df_1[col] = df_1[col].parallel_apply( lambda x: label2id[x])
 
     source_list = []
     target_list = []
     value_list = []
 
     for a,b in zip(domain_subset[:-1],domain_subset[1:]):
-        _df_ = df_1[[a,b]].groupby([a,b]).count().reset_index(name="count")
+       
+        _df_ = df_1[[a,b]].groupby([a,b]).size().reset_index(name='count')
         source_list.extend(_df_[a].values.tolist())
         target_list.extend(_df_[b].values.tolist())
         value_list.extend(_df_['count'].values.tolist())
-
+        print(len(target_list), len(source_list), len(value_list))
+        
     if diagram_type == 1:
         title = 'Sankey Diagram'
     elif diagram_type == 2:
@@ -118,9 +132,10 @@ def get_sankey_diagram(
     # -----------------
     # create a graph for sankey diagram
     # -----------------
-
+  
 
     label_list = list(all_vals)
+    label_list = [ _.split(':') for _ in label_list]
     link_source = source_list
     link_target = target_list
 
@@ -133,9 +148,9 @@ def get_sankey_diagram(
             label=label_list
         ),
         link=dict(
-            source=link_source,  # indices correspond to labels, eg A1, A2, A1, B1, ...
-            target=link_target,
-            value=[5, 4, 2, 2, 4, 2]
+            source= link_source,  # indices correspond to labels, eg A1, A2, A1, B1, ...
+            target= link_target,
+            value= value_list
         ))])
 
     fig.update_layout(
