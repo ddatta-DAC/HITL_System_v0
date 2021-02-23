167fa6096b49757c0057ab4dfd324d96582ef3f3
Debanjan Datta <ddatta@vt.edu>
2021-02-23 14:46:04 -0500

    update

--- VisualComponents_backend/TimeSeries/fetchTimeSeries.py
+++ VisualComponents_backend/TimeSeries/fetchTimeSeries.py
@@ -6,7 +6,7 @@ import os
 from plotly.graph_objs import *
 import plotly.io as pio
 from pathlib import Path
-
+from redisStore import redisUtil
 sys.path.append('./../..')
 from DB_Ingestion.sqlite_engine import sqlite
 
@@ -15,8 +15,8 @@ DATA_LOC = None
 subDIR = None
 html_saveDir = None
 json_saveDir = None
-redis_obj = None
 
+redis_obj = redisUtil.redisStore
 
 def initialize(
         _DATA_LOC=None,
@@ -25,18 +25,19 @@ def initialize(
         _json_saveDir=None
 ):
     global redis_obj, DATA_LOC, subDIR, html_saveDir, json_saveDir
+    
     DATA_LOC = _DATA_LOC
     subDIR = _subDIR
     redis_obj.ingest_record_data(DATA_LOC, subDIR)
 
     if _html_saveDir is None:
-        _html_saveDir = './Cache_sankeyHTML'
+        _html_saveDir = './htmlCache'
     html_saveDir = _html_saveDir
     Path(_html_saveDir).mkdir(exist_ok=True, parents=True)
     if _json_saveDir is None:
         json_saveDir = './jsonCache'
     else:
-        json_saveDir = json_saveDir
+        json_saveDir = _json_saveDir
 
     Path(json_saveDir).mkdir(exist_ok=True, parents=True)
     return
@@ -57,9 +58,9 @@ return_type:
 
 def fetchTS(
         domain='ConsigneePanjivaID',
-        id=None,
-        dateColumn='',
-        table='',
+        _id=None,
+        dateColumn='ArrivalDate',
+        table='Records',
         title=None,
         use_cache=True,
         return_type=3
@@ -68,17 +69,18 @@ def fetchTS(
     global DATA_LOC
     global subDIR
     global html_saveDir, json_saveDir
+    
     signature = 'TimeSeries_{}_{}'.format(domain, id)
-    json_fname = os.path.join(json_saveDir, signature + '.json')
+    json_fpath = os.path.join(json_saveDir, signature + '.json')
     fig = None
 
-    if use_cache and os.path.exists(json_fname):
-        with open(json_fname, 'r') as f:
+    if use_cache and os.path.exists(json_fpath):
+        with open(json_fpath, 'r') as f:
             fig = pio.from_json(f.read())
-
+    
     if fig is None:
         df = pd.read_sql(
-            "select {},{} from {} where {}={}".format(domain, dateColumn, table, domain, id), SQL_conn,
+            "select {},{} from {} where {}={}".format(domain, dateColumn, table, domain, _id), SQL_conn,
             index_col=None
         )
 
@@ -95,29 +97,37 @@ def fetchTS(
             df,
             x="Date",
             hover_data={"Date": "|%B "},
-            title=title
+            title=title,
+            color_continuous_scale= px.colors.sequential.Viridis,
         )
         fig.update_xaxes(
             dtick="M1",
             tickformat="%b\n%Y",
             ticklabelmode="period"
         )
-        fig.update_xaxes(rangeslider_visible=True, )
-        fig.update_layout(paper_bgcolor='rgb(255, 252, 260)')
+        fig.update_xaxes(rangeslider_visible=True)
+        
         fig.update_layout(
             font_family="Courier New",
             font_color="DarkSlateGrey",
             title_font_family="Times New Roman",
             title_font_color="red",
             legend_title_font_color="green",
-            font_size=15
+            font_size=16,
+            paper_bgcolor='rgba(255, 255, 255 ,0.5)',
+            plot_bgcolor='rgba(255,240,240,0.1)',
+            xaxis_range=['2015-01-01','2017-12-31']
+        )
+        fig.update_xaxes(
+            dtick="M1",
+            tickformat="%b %Y",
+            ticklabelmode="period"
         )
 
         if use_cache:
             fig_json = pio.to_json(fig)
             fname = signature + '.json'
-            fig_json_path = os.path.join(json_saveDir, fname)
-            with open(fig_json_path, 'w') as f:
+            with open(json_fpath, 'w') as f:
                 f.write(fig_json)
 
     if return_type == 1:
@@ -126,13 +136,48 @@ def fetchTS(
         html_String = pio.to_html(fig, include_plotlyjs='cdn', include_mathjax='cdn', full_html=False)
         return html_String
     elif return_type == 3:
-        fname = signature + '.json'
+        fname = signature + '.html'
         fpath = os.path.join(html_saveDir, fname)
         fig.write_html(
             fpath, include_plotlyjs='cdn', include_mathjax='cdn', full_html=False
         )
         return fpath
     return fig
+
+
+
+def get_TimeSeries(
+    PanjivaRecordID,
+    return_type=3,
+    use_cache=True
+):
+    global SQL_conn
+    global DATA_LOC
+    global subDIR
+    
+    df = pd.read_sql(
+            "select {},{},{} from Records where {}={}".format('PanjivaRecordID', 'ConsigneePanjivaID', 'ShipperPanjivaID', 'PanjivaRecordID', str(int(PanjivaRecordID))), SQL_conn,
+            index_col=None
+    )
+    consignee = str(int(df['ConsigneePanjivaID'].values[0]))
+    shipper = str(int(df['ShipperPanjivaID'].values[0]))
+    
+    value_1 = fetchTS(
+        domain='ConsigneePanjivaID',
+        _id=consignee,
+        title='Activity of Consignee',
+        use_cache=use_cache,
+        return_type=return_type
+    )
+    
+    value_2 = fetchTS(
+        domain='ShipperPanjivaID',
+        _id=shipper,
+        title='Activity of Shipper',
+        use_cache=use_cache,
+        return_type=return_type
+    )
+    return value_1, value_2
 
 
 # ==================================
